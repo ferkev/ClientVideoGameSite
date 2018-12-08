f17a65bd1f5a96ae44eff7ca47fca86cb0837165
Kevin Le Franc <kevinlefranc@MacBook-Pro-de-Kevin.local>
2018-12-08 18:43:41 +0100

    test achitecture

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Consumer } from '../../Context';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <Consumer>
+        {
+          value => {
+
+            return(
+              <React.Fragment>
+                <a href="/">Logo</a>
+                <form>
+                  <input placeholder="Votre recherche..."/>
+                </form>
+                <a href="/login">Login</a>
+                <a href="/Register">Register</a>
+                <a href="/logout">
+                  <i className="material-icons">power_settings_new</i>
+                </a>
+              </React.Fragment>
+            )
+          }
+        }
+      </Consumer>
+     
+    )
+  }
+}
